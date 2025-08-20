3c7289c14b5666947fc30775b1b9c461f1843023
Sc0rpX <mahfuzurrahman5609@gmail.com>
2025-08-21 02:07:25 +0600

    Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Test code")
+function getComputerChoice(){
+    // Create an array of rock, paper and scissor
+    const options = ["rock", "paper", "scissor"]
+    // Generate a random number among 0, 1 and 2
+    const randomIndex = Math.floor(Math.random() * 3)
+    // Return the choice from the array using the random index 
+    return options[randomIndex]
+}
+
+console.log(getComputerChoice())
