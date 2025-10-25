69992a0673523621773fb26580adb5f53959a2b6
Sc0rpX <mahfuzurrahman5609@gmail.com>
2025-10-25 13:35:24 +0600

    Implement UI

--- script.js
+++ script.js
@@ -1,56 +1,108 @@
+// Variables
+let round = 1
+let playerScore = 0
+let computerScore = 0
+const icons = ["👊", "🖐️", "✌️"]
+
+// DOM Selectors
+const buttons = document.querySelector("#buttons")
+const result = document.querySelector("#result")
+const resultDes = document.querySelector("#result-description")
+const roundDis = document.querySelector("#round-indicator")
+const playerScoreDisplay = document.querySelector("#playerscore")
+const computerScoreDisplay = document.querySelector("#computerscore")
+const playerSign = document.querySelector("#playersign")
+const computerSign = document.querySelector("#computersign")
+const overlay = document.querySelector("#overlay")
+const finalResult = document.querySelector("#finalResult")
+const playAgain = document.querySelector("#playAgain")
+
+// Returns Random Choice of Computer
 function getComputerChoice(){
-    // Create an array of rock, paper and scissor
-    const options = ["rock", "paper", "scissor"]
-    // Generate a random number among 0, 1 and 2
-    const randomIndex = Math.floor(Math.random() * 3)
-    // Return the choice from the array using the random index 
-    return options[randomIndex]
+    const options = ["rock", "paper", "scissor"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return options[randomIndex];
 }
 
-function getHumanChoice(){
-    // Prompt the user to choose
-    const choice = prompt("Choose your move (rock, paper or scissor):")
-    // Return the value choosen by the user
-    return choice.toLowerCase()
+// Change the choosed icons
+function changeIcon(playerChoice, computerChoice){
+    switch (playerChoice){
+        case "rock":
+            playerSign.textContent = icons[0];
+            break;
+        case "paper":
+            playerSign.textContent = icons[1];
+            break;
+        case "scissor":
+            playerSign.textContent = icons[2];
+            break;
+    }
+
+    switch (computerChoice){
+        case "rock":
+            computerSign.textContent = icons[0];
+            break;
+        case "paper":
+            computerSign.textContent = icons[1];
+            break;
+        case "scissor":
+            computerSign.textContent = icons[2];
+            break;
+    }
 }
 
-function playGame(){
-    let humanScore = 0
-    let computerScore = 0
-
-    function playRound(humanChoice, computerChoice){
-        if ((humanChoice === "rock" && computerChoice === "scissor") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissor" && computerChoice === "paper")){
-            ++humanScore;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.\n `)
-        }
-        else if((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissor") || (humanChoice === "scissor" && computerChoice === "rock")){
-            ++computerScore
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.\n `)
-        }
-        else{
-            console.log(`It's a Draw! You both choose ${humanChoice}\n `)
-        }
-
-        console.log(`Your Score: ${humanScore}\nComputer's Score: ${computerScore}\n `)
+// Play a single round
+function playRound(playerChoice, computerChoice){
+    if ((playerChoice === "rock" && computerChoice === "scissor") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissor" && computerChoice === "paper")){
+        ++playerScore;
+        result.textContent = `You win!` 
+        resultDes.textContent = `${playerChoice} beats ${computerChoice}.`
+    }
+    else if((playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "paper" && computerChoice === "scissor") || (playerChoice === "scissor" && computerChoice === "rock")){
+        ++computerScore
+        result.textContent = `You lose!` 
+        resultDes.textContent = `${computerChoice} beats ${playerChoice}.`
+    }
+    else{
+        result.textContent = `It's a Draw!` 
+        resultDes.textContent = `You both choose ${playerChoice}\n `
     }
 
-    function getWinner(humanScore, computerScore){
-        if(humanScore > computerScore){
-            console.log(`You Won The Game!`)
-        }
-        else if(computerScore > humanScore){
-            console.log(`You lost The Game`)
-        }
-        else{
-            console.log("It's a Draw!")
-        }
-    }
-
-    for(let i = 1; i <= 5; i++){
-        playRound(getHumanChoice(), getComputerChoice())
-    }
-
-    getWinner(humanScore, computerScore)
+    changeIcon(playerChoice, computerChoice)
+    roundDis.textContent = `Round: ${round}`
+    playerScoreDisplay.textContent = `Your Score: ${playerScore}` 
+    computerScoreDisplay.textContent = `Computer's Score: ${computerScore}`
+    round++;
 }
 
-playGame()
+// Detects winner
+function getWinner(playerScore, computerScore){
+    if(playerScore > computerScore){
+        finalResult.textContent = `You Won The Game!`
+    }
+    else{
+        finalResult.textContent = `You lost The Game!`
+    }
+}
+
+// Detects if anyone reaches final score or not
+function isGameOver(){
+    return (playerScore == 5 || computerScore == 5);
+}
+
+buttons.addEventListener("click", (e) => {
+    const playerChoice = e.target.id
+
+    playRound(playerChoice, getComputerChoice())
+
+    if(isGameOver()){
+        getWinner(playerScore, computerScore)
+        overlay.style.display = "flex";
+
+        playAgain.addEventListener("click", () => {
+            location.reload()
+        })
+    }
+})
+
+
